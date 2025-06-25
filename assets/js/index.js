b67747b868dd349c290413bdecd4d048114601b1
Harold DAYE <dayeharold885@gmail.com>
2025-06-25 04:10:42 +0100

    Réalisation de la page acceuil

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,17 +19,45 @@
     activerDislike();
   };
 
+    //   foncton  d'activation de like 
+
   function activerLike() {
     btnlike.classList.replace('btn-outline-primary', 'btn-primary');
-    btndislike.classList.replace('btn-secondary', 'btn-outline-secondary');
+    btndislike.classList.replace('btn-primary', 'btn-outline-primary');
     btnlike.innerHTML = '<i class="bi bi-hand-thumbs-up-fill"></i> J’aime';
     btndislike.innerHTML = '<i class="bi bi-hand-thumbs-down"></i> Je n’aime pas';
   }
 
+     //   foncton  d'activation de dislike 
+
   function activerDislike() {
-    btndislike.classList.replace('btn-outline-secondary', 'btn-secondary');
+    btndislike.classList.replace('btn-outline-primary', 'btn-primary');
     btnlike.classList.replace('btn-primary', 'btn-outline-primary');
     btndislike.innerHTML = '<i class="bi bi-hand-thumbs-down-fill"></i> Je n’aime pas';
     btnlike.innerHTML = '<i class="bi bi-hand-thumbs-up"></i> J’aime';
   }
+
+
+
+  const togglecontentbtn = document.getElementById("togglecommentbtn");
+  const commentSection = document.getElementById("commentSection");
+  const commentList = document.getElementById("commentList");
+  const comment = document.getElementById("comment");
+  const commentsubmit = document.getElementById("commentsubmit");
+
+  togglecontentbtn.addEventListener("click", () => {
+    commentSection.classList.toggle("d-none");
+  });
+
+    Commentsubmit.addEventListener("click", () => {
+    const comment = comment.value.trim();
+    console.log(comment)
+    if (comment === "") {
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      li.innerText = comment;
+      commentList.appendChild(li);
+      comment.value = "";
+    }
+  });
 
